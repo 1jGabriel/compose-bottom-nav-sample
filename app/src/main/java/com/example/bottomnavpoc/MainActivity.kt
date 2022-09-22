a25ac23e91e03ab77d4bfa8fdff8d724c38f58c5
João Sousa <gsantana152@gmail.com>
2022-09-22 09:54:58 -0300

    committing project

---
+++ app/src/main/java/com/example/bottomnavpoc/MainActivity.kt
@@ -0,0 +1,157 @@
+package com.example.bottomnavpoc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.bottomnavpoc.ui.theme.BottomNavPocTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BottomNavPocTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+    Scaffold(
+        topBar = { TopAppBar(title = {Text("Bottom Navigation Demo")})  },
+        content = { NavigationHost(navController = navController) },
+        bottomBar = { BottomNavigationBar(navController = navController)}
+    )
+}
+
+@Composable
+fun NavigationHost(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = NavRoutes.Home.route,
+    ) {
+        composable(NavRoutes.Home.route) {
+            Home()
+        }
+
+        composable(NavRoutes.Contacts.route) {
+            Contacts()
+        }
+
+        composable(NavRoutes.Favorites.route) {
+            Favorites()
+        }
+    }
+}
+
+@Composable
+fun BottomNavigationBar(navController: NavHostController) {
+
+    BottomNavigation {
+        val backStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = backStackEntry?.destination?.route
+
+        NavBarItems.BarItems.forEach { navItem ->
+            BottomNavigationItem(
+                selected = currentRoute == navItem.route,
+                onClick = {
+                    navController.navigate(navItem.route) 
+                },
+
+                icon = {
+                    Icon(imageVector = navItem.image,
+                        contentDescription = navItem.title)
+                },
+                label = {
+                    Text(text = navItem.title)
+                },
+            )
+        }
+
+    }
+}
+
+@Composable
+fun Home() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Home,
+            contentDescription = "home",
+            tint = Color.Blue,
+            modifier = Modifier
+                .size(150.dp)
+                .align(Alignment.Center)
+        )
+    }
+}
+
+@Composable
+fun Contacts() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Face,
+            contentDescription = "contacts",
+            tint = Color.Blue,
+            modifier = Modifier
+                .size(150.dp)
+                .align(Alignment.Center)
+        )
+    }
+}
+
+@Composable
+fun Favorites() {
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Favorite,
+            contentDescription = "favorites",
+            tint = Color.Blue,
+            modifier = Modifier
+                .size(150.dp)
+                .align(Alignment.Center)
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BottomNavPocTheme {
+        Greeting("Android")
+    }
+}
